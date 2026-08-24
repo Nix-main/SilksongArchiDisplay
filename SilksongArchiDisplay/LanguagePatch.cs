7e5c4d1631321349cd6bbdc468ede8ae2942a520
Nix-main <amberthepixel@gmail.com>
2026-08-23 21:40:17 -0400

    gorb

---
+++ SilksongArchiDisplay/LanguagePatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using TeamCherry.Localization;
+
+namespace SilksongArchiDisplay;
+
+[HarmonyPatch]
+public class LanguagePatch
+{
+    [HarmonyPatch(typeof(Language), nameof(Language.Get), typeof(string), typeof(string))]
+    [HarmonyPrefix]
+    public static bool GetPatch(string key, ref string __result)
+    {
+        if (key.Equals("TUTORIAL_CREDIT_NAME_03"))
+        {
+            __result = "Jasmine Vine";
+            return false;
+        }
+
+        return true;
+    }
+}
