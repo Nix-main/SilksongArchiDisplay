c36779f8453488764793a6c0681861fa5118c607
Sky <git@auroramoonlight.dev>
2026-08-31 21:02:52 -0600

    fix: add a concurrent queue to fix Display.AddItem thread crash

--- SilksongArchiDisplay/ArchipelagoPatch.cs
+++ SilksongArchiDisplay/ArchipelagoPatch.cs
@@ -1,6 +1,5 @@
 ﻿using Archipelago.MultiClient.Net.Models;
 using HarmonyLib;
-using RecentItemsDisplay;
 using SilksongRandomizer;
 using Archi = SilksongRandomizer.Archipelago;
 
@@ -16,7 +15,8 @@ public class ArchipelagoPatch
     {
         __instance.OnItemSent += (item, _) =>
         {
-            Display.AddItem(RandomizerPlugin.Instance.FillerIcon, $"{item.Split(" to ")[0].Replace("_", " ")}\nto {item.Split(" to ")[1]}");
+            // Utilize the queue system from SilksongArchiDisplay to queue the items instead of directly adding them.
+            SilksongArchiDisplayPlugin.QueueItem(RandomizerPlugin.Instance.FillerIcon, $"{item.Split(" to ")[0].Replace("_", " ")}\nto {item.Split(" to ")[1]}");
         };
     }
     
@@ -25,6 +25,7 @@ public class ArchipelagoPatch
     {
         string name = item.Player.Name == "Server" ? "Start" : item.Player.Name;
         if (raiseEvent)
-            Display.AddItem(RandomizerPlugin.Instance.MapCheckIcon,$"{__instance.GetItemName(item).Replace("_", " ")}\nfrom {name}");
+            // Utilize the queue system from SilksongArchiDisplay to queue the items instead of directly adding them.
+            SilksongArchiDisplayPlugin.QueueItem(RandomizerPlugin.Instance.MapCheckIcon,$"{__instance.GetItemName(item).Replace("_", " ")}\nfrom {name}");
     }
 }
--- SilksongArchiDisplay/SilksongArchiDisplay.cs
+++ SilksongArchiDisplay/SilksongArchiDisplay.cs
@@ -5,6 +5,7 @@ using BepInEx.Logging;
 using HarmonyLib;
 using RecentItemsDisplay;
 using SilksongRandomizer;
+using System.Collections.Concurrent;
 
 namespace SilksongArchiDisplay;
 
@@ -14,10 +15,26 @@ namespace SilksongArchiDisplay;
 [BepInDependency(RandomizerPlugin.PluginGuid)]
 public partial class SilksongArchiDisplayPlugin : BaseUnityPlugin
 {
+    // Make a queue to release the items via Update() to avoid releases and large item checks from crashing the game while running or on reconnect.
+    private static readonly ConcurrentQueue<(UnityEngine.Sprite sprite, string message)> pendingItemsQueue = new();
+
     void Awake()
     {
         new Harmony(Id).PatchAll();
         VanillaItems.mgr.Dispose();
         VanillaItems.fsmMgr.Dispose();
+    }
+
+    void Update()
+    {
+        while (pendingItemsQueue.TryDequeue(out var item))
+        {
+            Display.AddItem(item.sprite, item.message);
+        }
+    }
+
+    internal static void QueueItem(UnityEngine.Sprite sprite, string message)
+    {
+        pendingItemsQueue.Enqueue((sprite, message));
     }
 }
